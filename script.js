ad55b68079e992006675f1549462ff9d72ad9bc2
ali <seyedali1379aghaei@gmail.com>
2024-11-23 11:44:21 -0800

    first commit

---
+++ script.js
@@ -0,0 +1,4 @@
+document.getElementById('menu-toggle').addEventListener('click', function() {
+    const menu = document.getElementById('mobile-menu');
+    menu.classList.toggle('show'); // Toggle the 'show' class
+});
